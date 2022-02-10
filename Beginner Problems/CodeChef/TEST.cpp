0ece58c0e99ed61c96490ec5fee68cf41daef56d
sunami09 <sunamidasgupta@gmail.com>
2022-02-11 02:30:13 +0530

    Life, the Universe, and Everything

---
+++ Beginner Problems/CodeChef/TEST.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n != 42)
+    {
+        cout << n << endl;
+        cin >> n;
+    }
+    return 0;
+}
